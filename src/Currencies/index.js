6cc5a8d364e6f23f5af5cf85a3f7a93364422425
Aneta Borowski <borowski.aneta@gmail.com>
2023-01-11 21:17:46 +0100

    id removed from currencies

--- src/Currencies/index.js
+++ src/Currencies/index.js
@@ -1,16 +1,13 @@
 export const currencies = [
     { 
-      id: 1,
       name: "USD",
       rate: 4.4,
     },
     {
-      id: 2,
       name: "EUR",
       rate: 4.67,
     },
     {
-      id: 3,
       name: "NOK",
       rate: 0.46,
     },

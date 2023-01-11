713397c34ae5fe19b0f785277cb13b0e458aff93
Aneta Borowski <borowski.aneta@gmail.com>
2023-01-11 20:44:47 +0100

    Label component added to Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,11 @@
 import "./style.css";
-import Label from "../Label";
-import Result from "../Result";
 import { useState } from "react";
+import { currencies } from "../Currencies";
 
-const Form = ({ title, input, select }) => {
+const Form = (props) => {
 
   const [amount, setAmount] = useState("");
+  const [result, setResult] = useState("");
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -13,48 +13,46 @@ const Form = ({ title, input, select }) => {
 
   return (
     <form className="form" onSubmit={onFormSubmit}>
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walut</legend>
-            <Label
-                title="Wymieniam*"
-                input={
-                    <input
-                        value={amount}
-                        className="label__input label__input--pln"
-                        name="amount"
-                        type="number"
-                        min="1"
-                        placeholder="Podaj wartość w PLN"
-                        required
-                        onChange={({target}) => setAmount(target.value)}
-                    />
-                }
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator walut</legend>
+        <p>
+          <label className="label">
+            <span className="label__title">Wymieniam*</span>
+            <input
+              value={amount}
+              className="label__input label__input--pln"
+              name="amount"
+              type="number"
+              min="1"
+              placeholder="Podaj wartość w PLN"
+              required
+              onChange={({ target }) => setAmount(target.value)}
             />
-            <Label
-                title="Otrzymam"
-                input={
-                    <input
-                        className="label__input label__input--disabled"
-                        name="amount"
-                        type="number"
-                        min="1"
-                        disabled
-                    />
-                }
-                select={
-                    <select className="label__input">
-                        <option value="USD">$</option>
-                        <option value="€">€</option>
-                        <option value="NOK">NOK</option>
-                    </select>
-                }
+          </label>
+        </p>
+        <p>
+          <label className="label">
+            <span className="label__title">Otrzymam</span>
+            <input
+              className="label__input label__input--disabled"
+              name="amount"
+              type="number"
+              min="1"
+              disabled
             />
-
-            <button className="form__button">Przelicz</button>
-            <Result />
-            <p className="form__paragraph">*pole obowiązkowe</p>
-        </fieldset>
-    </form>);
+            <select className="label__input">
+              <option value="USD">$</option>
+              <option value="€">€</option>
+              <option value="NOK">NOK</option>
+            </select>
+          </label>
+        </p>
+        <button className="form__button">Przelicz</button>
+        <p className="form__result"></p>
+        <p className="form__paragraph">*pole obowiązkowe</p>
+      </fieldset>
+    </form>
+  )
 }
 
 export default Form;

701e2b01dabc5491550e35bf2afa63770a3b9831
Aneta Borowski <borowski.aneta@gmail.com>
2023-01-11 20:43:43 +0100

    Currencies component added

---
+++ src/Currencies/index.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    { 
+      id: 1,
+      name: "USD",
+      rate: 4.4,
+    },
+    {
+      id: 2,
+      name: "EUR",
+      rate: 4.67,
+    },
+    {
+      id: 3,
+      name: "NOK",
+      rate: 0.46,
+    },
+  ];

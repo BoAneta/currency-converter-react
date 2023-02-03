7e52f42895e89c3b54001530359714420ed847ab
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-03 15:20:06 +0100

    foematedDate function added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,15 +1,22 @@
-import {useCurrentDate} from "./useCurrentDate";
+import { useCurrentDate } from "./useCurrentDate";
 import { Calendar } from "./styled";
 
 export const Clock = () => {
     const date = useCurrentDate();
 
+    const formatedDate = (date) => {
+
+        return `${date.toLocaleString("pl-PL",
+            { weekday: "long", day: "numeric", month: "long" })}
+        ${", "}
+        ${date.toLocaleTimeString()}`
+    };
+
+
     return (
         <Calendar>
             Dzisiaj jest {" "}
-            {date.toLocaleString("pl-PL", {weekday: "long", day: "numeric", month: "long"})}
-            {", "}
-            {date.toLocaleTimeString()}
+            {formatedDate(date)}
         </Calendar>
     )
 };

e03cfea84533bceb5cf83ff8fd4d1117c85c5df9
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-03 12:41:36 +0100

    Styled components added to Result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,10 +1,12 @@
+import { StyledResult } from "./styled";
+
 export const Result = ({result}) => (
-    <p className="form__result">
+    <StyledResult>
         {result !== "" && (
             <>
             Wymieniasz <strong>{result.amount} PLN</strong>&nbsp; 
             i otrzymasz <strong>{result.resultValue.toFixed(2)} {result.currency}</strong>
             </>
         )}
-    </p>
+    </StyledResult>
 )
---
+++ src/Result/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledResult = styled.p`
+    text-align: center;
+    padding: 10px 0;
+`;

5222a91bb17b9f01739ff43d2950f7c788fc1531
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-04 16:50:31 +0100

    Result formated

--- src/Result/index.js
+++ src/Result/index.js
@@ -4,9 +4,9 @@ export const Result = ({result}) => (
     <StyledResult>
         {result !== "" && (
             <>
-            Wymieniasz <strong>{result.amount} PLN</strong>&nbsp; 
-            i otrzymasz <strong>{result.resultValue.toFixed(2)} {result.currency}</strong>
+                Wymieniasz <strong>{result.amount} PLN</strong>&nbsp; 
+                i otrzymasz <strong>{result.resultValue.toFixed(2)} {result.currency}</strong>
             </>
         )}
     </StyledResult>
-)
+);

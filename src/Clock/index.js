5e0d2d877e229270282c1388944c3af3120f02c2
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-05 14:52:32 +0100

    Clock formated

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -4,18 +4,22 @@ import { Calendar } from "./styled";
 export const Clock = () => {
     const date = useCurrentDate();
 
-    const formatedDate = (date) => {
-
-        return `${date.toLocaleString("pl-PL",
-            { weekday: "long", day: "numeric", month: "long" })}
-        ${", "}
-        ${date.toLocaleTimeString()}`
-    };
+    const formatedDate = (date) => date.toLocaleString(undefined, { 
+        weekday: "long", 
+        day: "numeric", 
+        month: "long",
+        hour: "2-digit",
+        minute:"2-digit",
+        second:"2-digit"
+    });
+    
+    ;
 
 
     return (
         <Calendar>
-            Dzisiaj jest {" "}
+            Dzisiaj jest 
+            {" "}
             {formatedDate(date)}
         </Calendar>
     )

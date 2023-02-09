dc868cac39763f2bc9b08dc2722281b3b24ce94f
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-09 19:43:48 +0100

    currencies updated to just USD

--- src/Form/currencies.js
+++ src/Form/currencies.js
@@ -1,14 +1,5 @@
 export const currencies = [
     { 
       name: "USD",
-      rate: 4.4,
-    },
-    {
-      name: "EUR",
-      rate: 4.67,
-    },
-    {
-      name: "NOK",
-      rate: 0.46,
     },
   ];

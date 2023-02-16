137533a1bfcefee92bff5dba8c948d7e3e2e03f9
Aneta Borowski <borowski.aneta@gmail.com>
2023-02-16 17:06:25 +0100

    code formated

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -4,21 +4,21 @@ import { Calendar } from "./styled";
 export const Clock = () => {
     const date = useCurrentDate();
 
-    const formatedDate = (date) => date.toLocaleString(undefined, { 
-        weekday: "long", 
-        day: "numeric", 
+    const formatedDate = (date) => date.toLocaleString(undefined, {
+        weekday: "long",
+        day: "numeric",
         month: "long",
         hour: "2-digit",
-        minute:"2-digit",
-        second:"2-digit"
+        minute: "2-digit",
+        second: "2-digit"
     });
-    
+
     ;
 
 
     return (
         <Calendar>
-            Dzisiaj jest 
+            Dzisiaj jest
             {" "}
             {formatedDate(date)}
         </Calendar>
--- src/Result/index.js
+++ src/Result/index.js
@@ -1,10 +1,10 @@
 import { StyledResult } from "./styled";
 
-export const Result = ({result}) => (
+export const Result = ({ result }) => (
     <StyledResult>
         {result !== "" && (
             <>
-                Wymieniasz <strong>{result.amount} PLN</strong>&nbsp; 
+                Wymieniasz <strong>{result.amount} PLN</strong>&nbsp;
                 i otrzymasz <strong>{result.resultValue.toFixed(2)} {result.currency}</strong>
             </>
         )}

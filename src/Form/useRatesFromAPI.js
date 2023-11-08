5ecd11a8f8db01c364cb598c6550a7ffc55c1954
Aneta Borowski <borowski.aneta@gmail.com>
2023-11-08 10:51:08 +0100

    api address changes, axios changed to fetch

--- src/Form/useRatesFromAPI.js
+++ src/Form/useRatesFromAPI.js
@@ -1,29 +1,39 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useRatesFromAPI = () => {
     const [ratesFromApi, setRatesFromAPI] = useState({
         state: "loading",
+        data: null,
     });
 
+const apiUrl = "https://api.currencyapi.com/v3/latest?apikey=cur_live_vM6Sk9h5YCC3x95ULZRQgxxxQ0Exwc130Pbop0EC&currencies=EUR%2CUSD%2CCAD&base_currency=PLN"
+
     useEffect(() => {
-        const getRates = async () => {
+        const fetchRates = async () => {
             try {
-                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const response = await fetch(apiUrl); 
+
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+
+                const { data, meta } = response.data;
 
                 setRatesFromAPI({
                     state: "success",
-                    rates: response.data.rates,
-                    date: response.data.date,
+                    data,
+                    meta,
                 });
-            } catch {
+
+            } catch (error) {
                 setRatesFromAPI({
                     state: "error",
+                    data: null,
                 });
             };
         };
 
-        setTimeout(getRates, 2000);
+        setTimeout(fetchRates, 2000);
     }, []);
 
     return ratesFromApi;
